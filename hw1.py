025bcefd339a486cdee7303e23dd841028c64eba
Gary Chiu <gchiu02@calpoly.edu>
2024-10-15 00:35:44 -0700

    parts 1-4 done

--- hw1.py
+++ hw1.py
@@ -1,18 +1,45 @@
 import data
+from data import Price
+
 
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def vowel_count(word:str) -> int:
+    tally = 0
+    for i in word:
+        if i.lower() in ['a','e','i','o','u']:
+            tally+=1
+    return tally
 
 # Part 2
-
+def short_lists(x:list[list[int]]) -> list:
+    short = []
+    for i in x:
+        if len(i) == 2:
+            short.append(i)
+    return short
 
 # Part 3
+def ascending_pairs(x:list[list[int]]) -> list:
+    ascending = []
+    for i in x:
+        if len(i) == 2:
+            if i[0]>i[1]:
+                ascending.append([i[1],i[0]])
+            else:
+                ascending.append(i)
+        else:
+            ascending.append(i)
+    return ascending
 
 
 # Part 4
-
+def add_prices(p1:Price, p2:Price):
+    p3 = Price(p1.dollars + p2.dollars, p1.cents + p2.cents)
+    p3.dollars += p3.cents // 100
+    p3.cents = p3.cents % 100
+    return p3
 
 # Part 5
 
--- hw1_test.py
+++ hw1_test.py
@@ -2,21 +2,65 @@ import data
 import hw1
 import unittest
 
+from data import Price
+
 
 # Write your test cases for each part below.
 
 class TestCases(unittest.TestCase):
     # Part 1
+    def test_vowel_count_1(self):
+        input = "aloha"
+        result = hw1.vowel_count(input)
+        expected = 3
+        self.assertEqual(expected, result)
 
+    def test_vowel_count_2(self):
+        input = "a smorgasbord"
+        result = hw1.vowel_count(input)
+        expected = 4
+        self.assertEqual(expected, result)
 
     # Part 2
+    def test_short_lists_1(self):
+        input = [[1],[2,3],[4,5,6],[7,8,9],[10,11]]
+        result = hw1.short_lists(input)
+        expected = [[2,3],[10,11]]
+        self.assertEqual(expected, result)
 
+    def test_short_lists_2(self):
+        input = [[2,1],[4,6],[4,3],[2,3,4]]
+        result = hw1.short_lists(input)
+        expected = [[2,1],[4,6],[4,3]]
+        self.assertEqual(expected, result)
 
     # Part 3
+    def test_ascending_pairs_1(self):
+        input = [[1], [2, 3], [4, 5, 6], [7, 8, 9], [12, 11]]
+        result = hw1.ascending_pairs(input)
+        expected = [[1], [2, 3], [4, 5, 6], [7, 8, 9], [11, 12]]
+        self.assertEqual(expected, result)
 
+    def test_ascending_pairs_2(self):
+        input = [[2, 1], [4, 6], [4, 3], [2, 3, 4]]
+        result = hw1.ascending_pairs(input)
+        expected = [[1, 2], [4, 6], [3, 4], [2, 3, 4]]
+        self.assertEqual(expected, result)
 
     # Part 4
+    def test_add_prices_1(self):
+        cost1 = Price(3,35)
+        cost2 = Price(10, 35)
+        result = hw1.add_prices(cost1, cost2)
+        expected = Price(13, 70)
+        self.assertEqual(expected, result)
 
+    def test_add_prices_2(self):
+        cost1 = Price(5, 55)
+        cost2 = Price(5, 969)
+        result = hw1.add_prices(cost1, cost2)
+        expected = Price(20, 24)
+        self.assertEqual(expected, result)
 
     # Part 5
 
